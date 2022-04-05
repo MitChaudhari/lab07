75175b88803b4e8f811d677c8acafe62e441dcb5
Mitansh Chaudhari <93405512+MitChaudhari@users.noreply.github.com>
2022-04-05 18:21:54 -0500

    Added the main code for JS

---
+++ js/script.js
@@ -0,0 +1,16 @@
+function init() {
+    //add your javascrip between these two lines of code
+    function alertFunction() {
+
+        var getInput = document.getElementById("entryinput").value;
+        alert("Mitansh Chaudhari: " + getInput);
+        document.getElementById("textoutput").innerHTML=getInput;
+    }
+
+    var actButton = document.getElementById("entrybutton");
+    actButton.addEventListener("click", alertFunction);
+
+
+
+}
+window.addEventListener('load', init);
